1d7a8b2d93899498b6d49af88cf5b479a52e5629
Vida <herling.codingcamp@gmail.com>
2022-10-06 12:45:36 -0400

    udpate success page

--- pages/success.js
+++ pages/success.js
@@ -1,11 +1,17 @@
+import { Card, Button } from "react-bootstrap";
+
 export default function Success(){
   return (
-    <div className="justify-to-end">
-      <h1>Thank you for your order</h1>
-      <h2>We have received your payment and will work on the order shortly.</h2>
-      <div>
-        <a href="/">Web page will redirect to homepage in 5 seconds.</a>
-      </div>
-    </div>
+    <>
+      <Card style={{ margin: "1rem 0" }}>
+        <Card.Title className="card-header">Thank you for your order</Card.Title>
+        <Card.Body>
+          <h4>We have received your payment and will work on the order shortly.</h4>
+          <div>
+            <a href="/">Web page will redirect to homepage in 5 seconds.</a>
+          </div>
+        </Card.Body>
+      </Card>
+    </>
   )
 }
